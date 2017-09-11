6c7d198334902e028fa827ff4f63a350e50008e7
Thomas Moser <mail@thomasmoser.ch>
2017-09-11 18:38:04 +0200

    Feat: Setup the project with redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
+//import reducer from './reducers' (TODO: Implement Reducers and Actions)
+import { Provider } from 'react-redux'
+// import logger from 'redux-logger' (TODO: Install and use redux-logger)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
