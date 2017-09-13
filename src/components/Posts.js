8ab7f865e293ce3d6a3109d5e5429fc9863c9222
Thomas Moser <mail@thomasmoser.ch>
2017-09-13 17:35:12 +0200

    feat: Added filtering posts by category

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,6 +5,9 @@ import Post from './Post'
 import { sortPosts } from '../actions'
 
 class Posts extends Component {
+
+  /*TODO: Why is Ordering made with redux store and why is filtering made with state in this component? Is there any reason for that?*/
+
   state = {
     activeCategoryItem: 'all categories',
   }
@@ -17,7 +20,7 @@ class Posts extends Component {
 
   render() {
     const { sortValue, sortOrder } = this.props.posts
-    const { activeCategoryItem, activeOrderByItem } = this.state
+    const { activeCategoryItem } = this.state
     return (
       <Grid padded>
         <Grid.Row>
@@ -39,7 +42,7 @@ class Posts extends Component {
         <Grid.Row>
           <Grid.Column width={12}>
 
-            { this.props.posts.items.map((post) => (
+            { this.props.posts.items.filter((post) => (activeCategoryItem === "all categories" || post.category===activeCategoryItem)).map((post) => (
               <Post key={post.id} post={post}/>
             ))}
 
@@ -47,17 +50,16 @@ class Posts extends Component {
           <Grid.Column width={4}>
             <Menu fluid vertical tabular='right'>
               <Menu.Item name='all categories' active={activeCategoryItem === "all categories"} onClick={this.handleCategoryItemClick} />
+
               { this.props.categories.items.map((category) => (
                 <Menu.Item key={category.name} name={category.name} active={activeCategoryItem === category.name} onClick={this.handleCategoryItemClick} />
-              ))
-              }
+              ))}
+              
             </Menu>
           </Grid.Column>
         </Grid.Row>
 
-
       </Grid>
-
 
     );
   }
